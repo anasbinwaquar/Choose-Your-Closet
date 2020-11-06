c77e6c0fc1e2d322f1ca71b0edf2b25b0b1b4aa3
ABDURRAFAY360 <abdurrafay360@gmail.com>
2020-11-06 22:16:34 +0500

    action beiong commented

--- app/Notifications/test.php
+++ app/Notifications/test.php
@@ -42,7 +42,7 @@ class test extends Notification
     {
         return (new MailMessage)
                     ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    //->action('Notification Action', url('/'))
                     ->line('Thank you for using our application!');
     }
 
