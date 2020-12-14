fc0fcde03b2b15fc794df389a56b93b1cbe1ab97
Umaima26 <rakia.shafique@gmail.com>
2020-12-15 01:26:30 +0500

    email in contact form to admin

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ContactUs;
 use Illuminate\Support\Facades\DB; 
-use App\Notifications\CustomerNotification;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\ContactUsNotification;
+
 
 class ContactController extends Controller
 {
@@ -20,6 +21,12 @@ class ContactController extends Controller
 	public function Contact(Request $req)
     {
         ContactUs::create($req->all());
+        $Email = 'abdurrafay360@gmail.com';
+        $Name = $req->name;
+        $Subject = $req->subject;
+        $QueryMail = $req->email;
+        $Query = $req->message;
+        Notification::route('mail',$Email)->notify(new ContactUsNotification($Name, $Subject, $QueryMail, $Query));
         $check = 1;
         return view('Pages.ContactUs')->with('check',$check);
     }
