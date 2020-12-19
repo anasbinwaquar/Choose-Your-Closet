a4e2d617440a0e44731bee6fabe65da468fb663f
Umaima26 <rakia.shafique@gmail.com>
2020-12-20 03:27:08 +0500

    Orders vvvvvvvvvvvvvvvvvvvvvvv

--- app/Http/Controllers/OrdersSellController.php
+++ app/Http/Controllers/OrdersSellController.php
@@ -23,13 +23,41 @@ class OrdersSellController extends Controller
     	$OldCart = session()->get('cart');
         $CurrentCart = new cart($OldCart);
         $order = Orders_Sell::latest()->first();
+        $customer_id = session()->get('customer_id');
         if($order==NULL)
         {
-            print_r('Nothing to show');
+            $order = 1;
             
-        }    
-        DB::insert('insert into orders_sell(OrderID, ProductID, CustomerID, Quantity, Delivery_Address, Total) values (?, ?, ?, ?, ?, ?)', [$product_id, $customer_id, $quant]);
+        }  
+        else
+        {
+            $order++;
+        }  
+        $products = $CurrentCart->items;
+        foreach ($products as $products) 
+        {
+            DB::insert('insert into orders_sell(OrderID, ProductID, CustomerID, Quantity, Delivery_Address, Total) values (?, ?, ?, ?, ?, ?)', [$order, $products['item']['id'], $customer_id, $products['qty'], 'abcdefgh', $products['price']]);
+        }
+       
     	return view('Pages.Checkout')->with(array('product'=> $CurrentCart->items))->with('product_cart',$CurrentCart->totalPrice);
+    }
+
+    public function ViewCheckout(Request $req)
+    {
+        // session()->flush();
+        if(session()->has('cart'))
+        {
+            $oldCart = session()->get('cart');
+            // print_r('rafayyy');
+        }
+        else
+        {
+            return redirect('/');
+        }
+        $OldCart = session()->get('cart');
+        $CurrentCart = new cart($OldCart);
+       
+        return view('Pages.Checkout')->with(array('product'=> $CurrentCart->items))->with('product_cart',$CurrentCart->totalPrice);
     }
 
 }
