adc431032cfa83e23725848e6a60e556011c198d
anasbinwaquar <anasbinwaquar@yahoo.com>
2020-12-21 00:39:29 +0500

    Added some stuff

---
+++ database/migrations/temp/2020_12_19_174155_create__orders__sell_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersSellTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_sell', function (Blueprint $table) {
+            $table->unsignedBigInteger('OrderID');
+            $table->unsignedBigInteger('CustomerID');
+            $table->unsignedBigInteger('ProductID');
+            $table->unsignedBigInteger('Quantity');
+            $table->string('Delivery_Address');
+            $table->unsignedBigInteger('Total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_sell');
+    }
+}
