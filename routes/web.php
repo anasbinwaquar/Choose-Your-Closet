292dfdbd9ca9966f9f43f647ebe8ad72cca1066e
ABDURRAFAY360 <abdurrafay360@gmail.com>
2020-12-21 20:49:59 +0500

    sizes_to_some_extend

--- routes/web.php
+++ routes/web.php
@@ -125,7 +125,7 @@ Route::get('/delete_print/{id}',[App\Http\Controllers\CustomizerController::clas
 
 //cart
 
-Route::get('/cart/{product_id}', [App\Http\Controllers\CartController::class, 'AddToCart'])->name('CartData');
+Route::post('/cart/{product_id}', [App\Http\Controllers\CartController::class, 'AddToCart'])->name('CartData');
 
 Route::get('/CustomerCart', [App\Http\Controllers\CartController::class, 'ViewCart']);
 
