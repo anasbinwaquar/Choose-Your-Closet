8f11d526f1aa17b8ea773a5a004c20a600428d85
Umaima26 <rakia.shafique@gmail.com>
2020-12-23 01:35:43 +0500

    asd

--- resources/views/Seller/SellerSignUp.blade.php
+++ resources/views/Seller/SellerSignUp.blade.php
@@ -3,11 +3,76 @@
 <head>
     
     <title>Seller Sign Up</title>
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+ <link rel="icon" href="{{asset('images/Closet.png')}}">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js">
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="css/CustomerSignupStyle.css">
+    <link rel="stylesheet" type="text/css" href="css/nav_styling.css">
     <link rel="stylesheet" href="css/SellerSignupStyle.css">
 </head>
 <body>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light" style="opacity: 0.8;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/" style="color: #24C6DC; font-weight: bold;"> <img src="{{asset('images/Closet.png')}}" alt="logo" width="100" id="logo">Virtual Clothing Store</a>
 
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active navclass">
+        <a class="nav-link navlink" href="/" style="color: #24C6DC;">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navlink" href="AboutUs" style="color: #24C6DC;">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navlink" href="/customize" style="color: #24C6DC;">Customizer</a>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+        <a class="nav-link navlink" href="#" style="color: #24C6DC;">Products</a>
+         <div class="dropdown-content">
+        <a href="/" class="link">Purchase</a>
+        <a href="/RentProducts" class="link">Rent</a>
+        </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+        <a class="nav-link navlink active" href="#" style="color: #24C6DC;">Sign Up</a>
+         <div class="dropdown-content">
+        <a href="/CustomerSignUp" class="link">Customer</a>
+        <a href="/SellerSignUp" class="link">Seller</a>
+        </div>
+        </div>
+      </li>
+       <li class="nav-item">
+        <div class="dropdown">
+        <a class="nav-link navlink" href="#" style="color: #24C6DC;">Login</a>
+         <div class="dropdown-content">
+        <a href="/CustomerLogin" class="link">Customer</a>
+        <a href="/SellerLogin" class="link">Seller</a>
+        </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navlink" href="/ContactUs" style="color: #24C6DC;">Contact Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <div class="input-group mb-4">
+            <input type="search" placeholder="Search..." aria-describedby="button-addon6" class="form-control border-info">
+            <div class="input-group-append">
+              <button id="button-addon6" type="submit" class="btn btn-info"><i class="fa fa-search"></i></button>
+            </div>
+          </div>
+    </form>
+</nav>
     <div class="main">
 
         <section class="signup">
@@ -45,19 +110,19 @@
                             <input type="text" class="form-input" name="Phone_Number" required id="PhoneNumber" placeholder="Phone Number" value="{{old('Phone_Number')}}"/>
                         </div>
                         <div class="form-group">
-                            <input type="text" value="{{old('Website_Name')}} class="form-input" name="Website_Name"  id="WebsiteName" placeholder="Website Name(Optional)"/>
+                            <input type="text" value="{{old('Website_Name')}}" class="form-input" name="Website_Name"  id="WebsiteName" placeholder="Website Name(Optional)"/>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-input" value="{{old('Brand_Name')}} name="Brand_Name" id="BrandName" placeholder="Brand Name(Optional)"/>
+                            <input type="text" class="form-input" value="{{old('Brand_Name')}}" name="Brand_Name" id="BrandName" placeholder="Brand Name(Optional)"/>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-input" name="Username" value="{{old('Username')}} required id="Username" placeholder="Username"/>
+                            <input type="text" class="form-input" name="Username" value="{{old('Username')}}" required id="Username" placeholder="Username"/>
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-input" name="Password" value="{{old('Password')}} required id="Password" placeholder="Password"/>
+                            <input type="password" class="form-input" name="Password" value="{{old('Password')}}" required id="Password" placeholder="Password"/>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-input" name="CNIC" id="CNIC" value="{{old('CNIC')}}"" required placeholder="CNIC : 42201-XXXXXXX-X"/>
+                            <input type="text" class="form-input" name="CNIC" id="CNIC" value="{{old('CNIC')}}" required placeholder="CNIC : 42201-XXXXXXX-X"/>
                         </div>
                         <!-- <div class="form-group">
                             <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
@@ -69,14 +134,53 @@
                        <!--  </div> -->
                     </form>
                     <p class="loginhere">
-                        Have already an account ? <a href="#" class="loginhere-link">Login here</a>
+                        Already have an account ? <a href="/SellerLogin" class="loginhere-link">Login here</a>
                     </p>
                 </div>
             </div>
         </section>
 
     </div>
-
- 
+<!-- 
+     <footer class="page-footer dark">
+      <img src="{{asset('images/Closet.png')}}" alt="Logo" style="float: right; margin-right: 200px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Customer Portal</h5>
+                    <ul>
+                        <li><a href="/CustomerSignUp">Sign Up</a></li>
+                        <li><a href="/CustomerLogin">Login</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Purchase Product</a></li>
+                        <li><a href="/RentProducts">Rent Product</a></li>
+                        <li><a href="/customize">Custom Orders</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Seller Portal</h5>
+                    <ul>
+                        <li><a href="/SellerSignUp">Sign Up</a></li>
+                        <li><a href="/SellerLogin">Login</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>About</h5>
+                    <ul>
+                        <li><a href="AboutUs">About Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="/ContactUs">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2020 Copyright Choose Your Closet <img src="{{asset('images/Closet.png')}}" alt="Logo" width="50"></p>
+        </div>
+    </footer> -->
 </body>
 </html>
