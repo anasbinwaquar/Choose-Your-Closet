2dabf0dfda3442984438b67899275c51c71a9c6c
anasbinwaquar <anasbinwaquar@yahoo.com>
2020-12-22 21:39:54 +0500

    Update OrdersSellController.php

--- app/Http/Controllers/OrdersSellController.php
+++ app/Http/Controllers/OrdersSellController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\cart;
 use Illuminate\Support\Facades\DB; 
 use App\Models\Orders_Sell;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -47,11 +48,30 @@ class OrdersSellController extends Controller
             foreach ($products as $products)
             {
 
-             printf($products['siz']);
+             // printf($products['siz']);
             
             DB::insert('insert into orders_sell(OrderID, CustomerID, ProductID, Size, Quantity, Delivery_Address, Total, Date) values(?, ?, ?, ?, ?, ?, ?, ?)', [$orderid, $customer_id, $products['item']['id'], $products['siz'], $products['qty'], $Delivery_Address, $products['price'], $date]);
-
             } 
+            if($products['siz']=='S'){
+                $prod=Product::where('id',$products['item']['id'])->first();
+                $q=$prod->quantity_small-$products['qty'];
+                Product::where('id',$products['item']['id'])->update(['quantity_small'=>$q]);
+            } 
+            else if($products['siz']=='M'){
+                $prod=Product::where('id',$products['item']['id'])->first();
+                $q=$prod->quantity_medium-$products['qty'];
+                Product::where('id',$products['item']['id'])->update(['quantity_medium'=>$q]);
+            } 
+            else if($products['siz']=='L'){
+                $prod=Product::where('id',$products['item']['id'])->first();
+                $q=$prod->quantity_large-$products['qty'];
+                Product::where('id',$products['item']['id'])->update(['quantity_large'=>$q]);
+            } 
+            else if($products['siz']=='XL'){
+                $prod=Product::where('id',$products['item']['id'])->first();
+                $q=$prod->quantity_extra_large-$products['qty'];
+                Product::where('id',$products['item']['id'])->update(['quantity_extra_large'=>$q]);
+            }
         }
        session()->forget('cart');
     	return redirect('/');
