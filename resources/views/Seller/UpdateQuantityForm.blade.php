dfab742eac015168b6deae414a2be5c7b8c9dcf2
anasbinwaquar <anasbinwaquar@yahoo.com>
2020-12-25 21:04:54 +0500

    Work

--- resources/views/Seller/UpdateQuantityForm.blade.php
+++ resources/views/Seller/UpdateQuantityForm.blade.php
@@ -3,28 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Seller Portal - Choose Your Closet</title>
+    <title>Seller Portal</title>
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="/assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="/assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="icon" href="{{asset('images/Closet.png')}}">
-
-<style>
-    
-    .active{
-        font-weight: bold;
-    }
-
-    ::selection
-    {
-        background-color: #000000;
-        color: #ffffff;
-    }
-    
-</style>
-
 </head>
 
 <body id="page-top">
@@ -124,4 +108,9 @@
     <script src="/assets/js/theme.js"></script>
 </body>
 
+        <footer class="bg-white sticky-footer">
+            <div class="container my-auto">
+                <div class="text-center my-auto copyright"><span>Copyright © Brand 2020</span></div>
+            </div>
+        </footer>
 </html>
